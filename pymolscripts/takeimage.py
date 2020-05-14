2bfa3cb119846752c5c3e87cbe76967c6484b358
theoportlock <zn.tportlock@gmail.com>
2020-05-14 17:29:10 +0100

    Cleaning up

--- pymolscripts/takeimage.py
+++ pymolscripts/takeimage.py
@@ -2,6 +2,7 @@ from pymol import cmd, stored, math
 	
 def takeimage ():
     cmd.ray(2400,2400)
+    cmd.set('ray_shadows','off')
     cmd.png("output.png",dpi=300)
 
 cmd.extend("takeimage", takeimage);
