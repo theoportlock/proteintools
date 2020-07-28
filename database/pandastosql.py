7ed749323e5d60e0528db3758a8659950114ed36
theoportlock <zn.tportlock@gmail.com>
2020-07-28 16:20:12 +0100

    More database upgrades

--- database/pandastosql.py
+++ database/pandastosql.py
@@ -1,9 +1,8 @@
-import sqlite3
+import sqlite3 as sl
 import pandas as pd
 
 df = pd.read_csv("Proteins.csv")
-df.to_sql("Proteins.db")
-conn = sqlite3.connect('sbdata.db')
-df.to_sql(con=conn, name="proteins", if_exists='append', index=True)
-c = conn.cursor()
-c.execute("SELECT * from proteins;").fetchall()
+conn = sl.connect('sbdata.db')
+df.to_sql("proteins", con=conn, if_exists='append', index=True)
+#c = conn.cursor()
+#c.execute("SELECT * from proteins;").fetchall()
